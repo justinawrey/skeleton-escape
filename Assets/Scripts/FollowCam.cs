45508baeb2b797365fd71bffb913e67178bfcd79
Justin Awrey <awreyjustin@gmail.com>
2023-03-15 14:22:46 +0100

    smoothSPeed~

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -2,14 +2,22 @@ using UnityEngine;
 
 public class FollowCam : MonoBehaviour
 {
-  public Transform player;
+  private Rigidbody2D rb;
+
+  public GameObject player;
   public Vector2 offset = new Vector2(0, 2);
   public float smoothSpeed = 10f;
 
+  private void Start()
+  {
+    rb = player.GetComponent<Rigidbody2D>();
+  }
+
   private void Update()
   {
-    Vector2 desiredPosition = (Vector2)player.position + offset;
-    Vector2 smoothedPosition = Vector2.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+    Vector2 desiredPosition = (Vector2)player.transform.position + offset;
+    float calculatedSmoothSpeed = rb.velocity.y >= -10 ? smoothSpeed : smoothSpeed * 2;
+    Vector2 smoothedPosition = Vector2.Lerp(transform.position, desiredPosition, calculatedSmoothSpeed * Time.deltaTime);
     transform.position = new Vector3(transform.position.x, smoothedPosition.y, transform.position.z);
   }
 }
